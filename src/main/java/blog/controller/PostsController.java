23b568657905cc414989154e32138380506b0601
Buchanan, Alec <Alec.Buchanan@teamaol.com>
2018-01-18 17:55:37 -0500

    Added Posts Controller and Posts Html page

--- src/main/java/blog/controller/PostsController.java
+++ src/main/java/blog/controller/PostsController.java
@@ -1,7 +1,25 @@
 package blog.controller;
 
+import blog.model.Post;
+import blog.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by abuchanan57 on 1/18/18.
  */
+@Controller
 public class PostsController {
+    @Autowired
+    PostService postService;
+
+    @RequestMapping("/posts/view/{id}")
+    public String view(@PathVariable("id") Long id, Model model){
+        Post post = postService.findById(id);
+        model.addAttribute("post", post);
+        return "posts/view";
+    }
 }
